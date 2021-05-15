8c17cfc19a2a39e281ee1ab34b916e25bda8a945
Mohammad Abdellatif <mohammad.abdellatif@progressoft.com>
2021-05-15 12:34:45 +0300

    feat: add mongodb and postgress

---
+++ postgres/create_table.sql
@@ -0,0 +1,12 @@
+create table people (
+	id SERIAL PRIMARY KEY,
+    name varchar(200),
+	age smallint,
+	street varchar(200),
+	city varchar(200),
+	state varchar(200),
+	zip varchar(10),
+	lng double precision,
+	lat double precision
+);
+/
